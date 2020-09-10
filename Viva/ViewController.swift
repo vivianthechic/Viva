94c168af46062bf5676d283b1e93c44e985cafe4
Vivian Zhang <vivian.s.zhang@gmail.com>
2020-09-10 15:37:29 -0700

    Initial Commit

---
+++ Viva/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Viva
+//
+//  Created by Vivian Zhang on 9/10/20.
+//  Copyright © 2020 PennAppsXXI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
