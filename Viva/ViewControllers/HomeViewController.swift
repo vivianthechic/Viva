7bff65e8832a526f957de7f3fb9ff7fc6488047f
Alyssatly <alyssatan2010@gmail.com>
2020-09-12 23:58:29 -0700

    Added search functionality

--- Viva/ViewControllers/HomeViewController.swift
+++ Viva/ViewControllers/HomeViewController.swift
@@ -14,6 +14,10 @@ class HomeViewController: UIViewController, MGLMapViewDelegate  {
     var source: MGLShapeSource!
     var timer = Timer()
     var searchButton:UIButton!
+    var bounds: MGLCoordinateBounds!
+    var myLongtitude: Double!
+    var myLatitude: Double!
+    var trackUser: Bool = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,11 +26,25 @@ class HomeViewController: UIViewController, MGLMapViewDelegate  {
         mapView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
         mapView.delegate = self
         mapView.tintColor = .lightGray
+        
+        if (trackUser == false){
+            let center = CLLocationCoordinate2D(latitude: myLatitude, longitude: myLongtitude)
+            // Starting point
+            mapView.setCenter(center, zoomLevel: 13, direction: 0, animated: false)
+            
+            // Colorado’s bounds
+            let northeast = CLLocationCoordinate2D(latitude: myLatitude + 2, longitude: myLongtitude + 3)
+            let southwest = CLLocationCoordinate2D(latitude: myLatitude - 2, longitude: myLongtitude - 3)
+            bounds = MGLCoordinateBounds(sw: southwest, ne: northeast)
+        }
+        
         view.addSubview(mapView)
         
         mapView.showsUserLocation = true
-        //mapView.setUserTrackingMode(.follow, animated: true) {
-        //}
+        if(trackUser){
+            mapView.setUserTrackingMode(.follow, animated: true) {}
+        }
+        
         addButton()
     }
     
@@ -47,9 +65,6 @@ class HomeViewController: UIViewController, MGLMapViewDelegate  {
     }
     
     @objc func searchButtonWasPressed(_ sender:UIButton){
-//        let searchView = storyboard?.instantiateViewController(withIdentifier: "SearchViewController")
-//        view.window?.rootViewController = searchView
-//        view.window?.makeKeyAndVisible()
         performSegue(withIdentifier: "searchScreen", sender: self)
     }
     
@@ -62,46 +77,46 @@ class HomeViewController: UIViewController, MGLMapViewDelegate  {
             
             // Create a heatmap layer.
             let heatmapLayer = MGLHeatmapStyleLayer(identifier: "wanderdrone", source: source)
-             
+            
             // Adjust the color of the heatmap based on the point density.
             let colorDictionary: [NSNumber: UIColor] = [
                 0.0: .clear,
-            0.1: .blue,
-            0.3: .cyan,
-            0.5: .green,
-            0.7: .yellow,
-            1: .red
+                0.1: .blue,
+                0.3: .cyan,
+                0.5: .green,
+                0.7: .yellow,
+                1: .red
             ]
             heatmapLayer.heatmapColor = NSExpression(format: "mgl_interpolate:withCurveType:parameters:stops:($heatmapDensity, 'linear', nil, %@)", colorDictionary)
-             
+            
             // Heatmap weight measures how much a single data point impacts the layer's appearance.
             heatmapLayer.heatmapWeight = NSExpression(format: "mgl_interpolate:withCurveType:parameters:stops:(mag, 'linear', nil, %@)",
-            [0: 0,
-            6: 1])
-             
+                                                      [0: 0,
+                                                       6: 1])
+            
             // Heatmap intensity multiplies the heatmap weight based on zoom level.
             heatmapLayer.heatmapIntensity = NSExpression(format: "mgl_interpolate:withCurveType:parameters:stops:($zoomLevel, 'linear', nil, %@)",
-            [0: 1,
-            10: 1])
+                                                         [0: 1,
+                                                          10: 1])
             heatmapLayer.heatmapRadius = NSExpression(format: "mgl_interpolate:withCurveType:parameters:stops:($zoomLevel, 'linear', nil, %@)",
-            [5: 10,
-            16: 100])
-             
+                                                      [5: 10,
+                                                       16: 100])
+            
             // The heatmap layer should be visible up to zoom level 9.
             heatmapLayer.heatmapOpacity = NSExpression(format: "mgl_step:from:stops:($zoomLevel, 0.75, %@)", [0: 0.75, 9: 0])
             style.addLayer(heatmapLayer)
-             
+            
             // Add a circle layer to represent the earthquakes at higher zoom levels.
             let circleLayer = MGLCircleStyleLayer(identifier: "circle-layer", source: source)
-             
+            
             let magnitudeDictionary: [NSNumber: UIColor] = [
-            0: .white,
-            0.5: .yellow,
-            2.5: UIColor(red: 0.73, green: 0.23, blue: 0.25, alpha: 1.0),
-            5: UIColor(red: 0.19, green: 0.30, blue: 0.80, alpha: 1.0)
+                0: .white,
+                0.5: .yellow,
+                2.5: UIColor(red: 0.73, green: 0.23, blue: 0.25, alpha: 1.0),
+                5: UIColor(red: 0.19, green: 0.30, blue: 0.80, alpha: 1.0)
             ]
             circleLayer.circleColor = NSExpression(format: "mgl_interpolate:withCurveType:parameters:stops:(mag, 'linear', nil, %@)", magnitudeDictionary)
-             
+            
             // The heatmap layer will have an opacity of 0.75 up to zoom level 9, when the opacity becomes 0.
             circleLayer.circleOpacity = NSExpression(format: "mgl_step:from:stops:($zoomLevel, 0, %@)", [0: 0, 9: 0.75])
             circleLayer.circleRadius = NSExpression(forConstantValue: 20)
@@ -111,7 +126,33 @@ class HomeViewController: UIViewController, MGLMapViewDelegate  {
             //REMOVE TIMER IF NOT NEEDED
             timer.invalidate()
             timer = Timer.scheduledTimer(timeInterval: 1.5, target: self, selector: #selector(updateUrl), userInfo: nil, repeats: true)
+            
         }
+    }
+    
+    func mapView(_ mapView: MGLMapView, shouldChangeFrom oldCamera: MGLMapCamera, to newCamera: MGLMapCamera) -> Bool {
+        
+        // Get the current camera to restore it after.
+        if(trackUser == false){
+            let currentCamera = mapView.camera
+            
+            // From the new camera obtain the center to test if it’s inside the boundaries.
+            let newCameraCenter = newCamera.centerCoordinate
+            
+            // Set the map’s visible bounds to newCamera.
+            mapView.camera = newCamera
+            let newVisibleCoordinates = mapView.visibleCoordinateBounds
+            
+            // Revert the camera.
+            mapView.camera = currentCamera
+            
+            // Test if the newCameraCenter and newVisibleCoordinates are inside self.bounds.
+            let inside = MGLCoordinateInCoordinateBounds(newCameraCenter, self.bounds)
+            let intersects = MGLCoordinateInCoordinateBounds(newVisibleCoordinates.ne, self.bounds) && MGLCoordinateInCoordinateBounds(newVisibleCoordinates.sw, self.bounds)
+            
+            return inside && intersects
+        }
+        return true
     }
     
     @objc func updateUrl() {
@@ -124,5 +165,10 @@ class HomeViewController: UIViewController, MGLMapViewDelegate  {
         timer.invalidate()
         timer = Timer()
     }
+    
+//    public func setCoor(lat: Double, long: Double){
+//        myLatitude = lat
+//        myLongtitude = long
+//    }
     
 }
--- Viva/ViewControllers/SearchViewController.swift
+++ Viva/ViewControllers/SearchViewController.swift
@@ -11,16 +11,59 @@ import CoreLocation
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var searchTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        findLocation()
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
     }
     
-    func findLocation(){
-        let url = URL(string: "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYWx5c3NhdGx5IiwiYSI6ImNrZXl1YjA3ODE5cTQyeWw5MXhneHNrYWYifQ.LvyPyfNVLi11Re0oNEpRmw")
+    func findLocation(location: String){
+        let theLocation = location.addingPercentEncoding(withAllowedCharacters:NSCharacterSet.urlQueryAllowed) ?? "Nothing"
+        if(theLocation != "Nothing"){
+            let urlString = String(format: "https://api.mapbox.com/geocoding/v5/mapbox.places/%@.json?access_token=pk.eyJ1IjoiYWx5c3NhdGx5IiwiYSI6ImNrZXl1YjA3ODE5cTQyeWw5MXhneHNrYWYifQ.LvyPyfNVLi11Re0oNEpRmw",theLocation)
+            let url = URL(string: urlString)!
+            
+            let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+            let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+            let task = session.dataTask(with: request) { (data, response, error) in
+               // This will run when the network request returns
+               if let error = error {
+                  print(error.localizedDescription)
+               } else if let data = data {
+                    let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                    let features = dataDictionary["features"] as! [[String:Any]]
+                    let coor = features[0]["geometry"] as! [String:Any]
+                    let coordinates = coor["coordinates"]! as! [Any]
+                    let logtitude = coordinates[0] as! Double
+                    let latitude = coordinates[1] as! Double
+                    print(logtitude,latitude)
+                
+                    let tabBarController = self.storyboard?.instantiateViewController(withIdentifier: "tabBC") as! UITabBarController
+                
+                    let naviViewController = tabBarController.viewControllers?[0] as! UINavigationController
+                    let homeViewController = naviViewController.viewControllers[0] as! HomeViewController
+                    homeViewController.myLatitude = latitude
+                    homeViewController.myLongtitude = logtitude
+                    homeViewController.trackUser = false
+                    self.view.window?.rootViewController = tabBarController
+                    self.view.window?.makeKeyAndVisible()
+               }
+            }
+            task.resume()
+        }
     }
+
+    @IBAction func searchPressed(_ sender: Any) {
+        if(searchTextField.text != ""){
+            findLocation(location: searchTextField.text!)
+            
+        }
+    }
+    
 
     /*
     // MARK: - Navigation
